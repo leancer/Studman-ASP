7f26baa91911dc09372fb331d1671752d970a977
leancer <kamiyabasamadi@gmail.com>
2020-01-12 23:46:59 +0530

    initial

---
+++ studman/Controllers/StudentController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using studman.Models;
+
+namespace studman.Controllers
+{
+    public class StudentController : Controller
+    {
+        //
+        // GET: /Student/
+
+        public ActionResult Index()
+        {
+            return Json(new {msg = "working" },JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult me()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult register(student stud)
+        {
+            if (ModelState.IsValid)
+            {
+
+                if (this.isEmailExist(stud.email))
+                {
+                    return Json(new { status = "fail", msg = "Email Already Exist" }, JsonRequestBehavior.AllowGet);
+                }
+
+                using (adamlye_studmanEntities st = new adamlye_studmanEntities())
+                {
+
+                    st.students.AddObject(stud);
+                    int res = st.SaveChanges();
+                    if (res > 0)
+                    {
+                        return Json(new { status = "success", msg = "register Successfull" }, JsonRequestBehavior.AllowGet);
+                    }
+                    else
+                    {
+                        return Json(new { status = "fail", msg = "not Able to Register" }, JsonRequestBehavior.AllowGet);
+                    }
+                }
+
+
+
+            }
+            else
+            {
+                return Json(new { status = "fail", msg = "invalid Request" }, JsonRequestBehavior.AllowGet);
+            }
+
+
+
+        }
+
+        [HttpPost]
+        public JsonResult login(student stud)
+        {
+            using (adamlye_studmanEntities st = new adamlye_studmanEntities())
+            {
+
+                var v = st.students.Where(a => a.email == stud.email).FirstOrDefault();
+                if (v != null)
+                {
+                    if (string.Compare(stud.password, v.password) == 0)
+                    {
+                        return Json(new { status = "Success", msg = "Login Success" }, JsonRequestBehavior.AllowGet);
+                    }
+                    else
+                    {
+                        return Json(new { status = "fail", msg = "Wrong Credential" }, JsonRequestBehavior.AllowGet);
+                    }
+                }
+                else
+                {
+                    return Json(new { status = "fail", msg = "Wrong Credential" }, JsonRequestBehavior.AllowGet);
+                }
+            }
+        }
+
+        private JsonResult Json()
+        {
+            throw new NotImplementedException();
+        }
+
+        [NonAction]
+        public bool isEmailExist(string email)
+        {
+            using (adamlye_studmanEntities se = new adamlye_studmanEntities())
+            {
+
+                var v = se.students.Where(a => a.email == email).FirstOrDefault();
+                return v != null;
+            }
+        }
+
+    }
+}
---
+++ studman/Views/Home/Index.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/Views/Shared/Site.Master" Inherits="System.Web.Mvc.ViewPage" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="TitleContent" runat="server">
+    Home Page
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <form id="form1" runat="server">
+    <h2><%: ViewData["Message"] %>
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" 
+            DataKeyNames="id" DataSourceID="SqlDataSource1">
+            <Columns>
+                <asp:BoundField DataField="id" HeaderText="id" InsertVisible="False" 
+                    ReadOnly="True" SortExpression="id" />
+                <asp:BoundField DataField="email" HeaderText="email" SortExpression="email" />
+                <asp:BoundField DataField="password" HeaderText="password" 
+                    SortExpression="password" />
+            </Columns>
+        </asp:GridView>
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" 
+            ConnectionString="<%$ ConnectionStrings:adamlye_studmanConnectionString %>" 
+            SelectCommand="SELECT * FROM [student]"></asp:SqlDataSource>
+    </h2>
+    <p>
+        To learn more about ASP.NET MVC visit <a href="http://asp.net/mvc" title="ASP.NET MVC Website">http://asp.net/mvc</a>.
+        </p>
+</form>
+</asp:Content>
